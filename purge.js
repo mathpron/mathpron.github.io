f13f3ae5e9f9e080965452a2d66087e97b91683c
abccsss <buchenjing@outlook.com>
2020-07-11 14:25:57 +0800

    Improve purging workflow

--- purge.js
+++ purge.js
@@ -5,12 +5,17 @@ const jsDelivrPurge = 'https://purge.jsdelivr.net/gh/mathpron/mathpron.github.io
 
 function purge(file) {
     return new Promise((resolve, reject) => {
+        let timeout = setTimeout(function () {
+            console.log(`! Failed to purge ${file}.`);
+        }, 20000);
+
         request({
             url: jsDelivrPurge + '/' + file,
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'
             }
         }, (error, _response, _body) => {
+            clearTimeout(timeout);
             if (error) {
                 reject(error);
             } else {
@@ -22,7 +27,7 @@ function purge(file) {
 
 async function purgeAll() {
     let files = fs.readdirSync('.');
-    let exclude = [ 'build.js', 'purge.js' ];
+    let exclude = [ 'build.js', 'purge.js', 'package.json' ];
     let toPurge = [];
     files.forEach(file => {
         // all files on jsdelivr except /fonts/* and /img/* are purged.
@@ -33,6 +38,7 @@ async function purgeAll() {
 
     for (let i = 0; i < toPurge.length; i++) {
         let file = toPurge[i];
+        console.log(`Purging ${file}...`);
         await purge(file);
     }
 }
